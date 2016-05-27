0664eafdd326afe236ca29b25c553a49b4cca034
qimuyunduan <84607842@qq.com>
2016-05-27 11:21:26 +0800

    iOS client for ido

---
+++ ido_ios/ido_ios-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  ido_ios-Bridging-Header.h
+//  ido_ios
+//
+//  Created by qimuyunduan on 16/5/25.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+#ifndef ido_ios_Bridging_Header_h
+#define ido_ios_Bridging_Header_h
+
+#import <UMMobClick/MobClick.h>
+#import <SMS_SDK/SMSSDK.h>
+
+#endif /* ido_ios_Bridging_Header_h */
