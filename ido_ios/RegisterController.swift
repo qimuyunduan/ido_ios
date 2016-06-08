f75d6d94c1a0e7de54c886364da98b68fb749023
qimuyunduan <84607842@qq.com>
2016-06-08 15:16:38 +0800

    change main.storyboard

---
+++ ido_ios/RegisterController.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterController.swift
+//  ido_ios
+//
+//  Created by qimuyunduan on 16/6/8.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+import UIKit
+class RegisterController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ido_ios/SetPwdController.swift
@@ -0,0 +1,14 @@
+//
+//  SetPwdController.swift
+//  ido_ios
+//
+//  Created by qimuyunduan on 16/6/8.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+import UIKit
+class SetPwdController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
