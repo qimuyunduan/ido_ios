376b3c6042a3b566e66e04578d602af676f03d40
qimuyunduan <84607842@qq.com>
2016-06-09 11:00:53 +0800

    main.storyboard

---
+++ ido_ios/HPDetailController.swift
@@ -0,0 +1,15 @@
+//
+//  HPDetailController.swift
+//  ido_ios
+//
+//  Created by qimuyunduan on 16/6/8.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+import UIKit
+
+class HPDetailController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
