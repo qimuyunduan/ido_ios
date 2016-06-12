cc80543c3491bab32491b866917f4663dba92f81
qimuyunduan <84607842@qq.com>
2016-06-12 20:47:53 +0800

    commit detail

--- ido_ios/HPDetailController.swift
+++ ido_ios/HPDetailController.swift
@@ -9,6 +9,36 @@
 import UIKit
 
 class HPDetailController: UITableViewController {
+    
+    
+    @IBOutlet weak var priceTag: UILabel!
+    
+    @IBOutlet weak var bargainPrice: UILabel!
+    
+    @IBOutlet weak var bookNow: UIButton!
+    
+    @IBOutlet weak var storeName: UILabel!
+    
+    @IBOutlet weak var storeAddress: UILabel!
+    
+    @IBOutlet weak var dial: UIImageView!
+    
+    @IBOutlet weak var bigOrder: UILabel!
+    
+    @IBOutlet weak var count: UILabel!
+    
+    @IBOutlet weak var bigPrice: UILabel!
+    
+    @IBOutlet weak var tipOneTitle: UILabel!
+    
+    @IBOutlet weak var tipOneInfo: UILabel!
+    
+    @IBOutlet weak var tipTwoTitle: UILabel!
+    
+    @IBOutlet weak var titleTwoInfo: UILabel!
+    
+    @IBOutlet weak var usage: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
