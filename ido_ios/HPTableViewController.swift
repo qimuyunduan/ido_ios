f3930dbaccf23db2e759c42b17249c34fe035ba6
qimuyunduan <84607842@qq.com>
2016-06-25 16:07:25 +0800

    most complete headerView

--- ido_ios/HPTableViewController.swift
+++ ido_ios/HPTableViewController.swift
@@ -12,40 +12,11 @@ import Cartography
 class HPTableViewController: UITableViewController {
     
     private let pageControl = UIPageControl()
-    private let scrollView  = UIScrollView()
-    let screen = UIScreen.mainScreen()
-    let headerView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width, height: 100))
     private let number  = 3
+    let screen = UIScreen.mainScreen()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.showsVerticalScrollIndicator   = false
-        scrollView.frame = screen.bounds
-        scrollView.pagingEnabled = true
-        scrollView.contentOffset = CGPointZero
-        scrollView.bounces = false
-        scrollView.delegate = self
-        scrollView.contentSize = CGSize(width: screen.bounds.width * CGFloat(number), height: 100.0)
-        pageControl.numberOfPages = number
-        pageControl.currentPage = 0
-        for index in 0 ..< number {
-            let imageView = UIImageView(image: UIImage(named: "GuideImage\(index+1)"))
-            imageView.frame = CGRect(x: screen.bounds.width * CGFloat(index), y: 0, width: screen.bounds.width, height: 100)
-            scrollView.addSubview(imageView)
-        }
-        headerView.addSubview(scrollView)
-        headerView.addSubview(pageControl)
-        
-        constrain(scrollView,pageControl){
-            
-            scrollView,pageControl in
-            pageControl.left == scrollView.left + 20
-            pageControl.bottom == scrollView.bottom - 20
-            
-        }
-        
-        self.tableView.tableHeaderView = headerView
+        self.addHeaderView(self.tableView)
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
     }
@@ -62,10 +33,68 @@ class HPTableViewController: UITableViewController {
         }
 
     }
-    
-    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return nil
-           }
+    func addHeaderView(tableView:UITableView) -> Void {
+     
+        let scrollView  = UIScrollView()
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: screen.bounds.width, height: 140))
+        let ADView = UIView(frame: CGRect(x: 0, y: 0, width: screen.bounds.width, height: 100))
+        
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator   = false
+        scrollView.frame = screen.bounds
+        scrollView.pagingEnabled = true
+        scrollView.contentOffset = CGPointZero
+        scrollView.bounces = false
+        scrollView.delegate = self
+        scrollView.contentSize = CGSize(width: screen.bounds.width * CGFloat(number), height: 100.0)
+        pageControl.numberOfPages = number
+        pageControl.currentPage = 0
+        for index in 0 ..< number {
+            let imageView = UIImageView(image: UIImage(named: "GuideImage\(index+1)"))
+            imageView.frame = CGRect(x: screen.bounds.width * CGFloat(index), y: 0, width: screen.bounds.width, height: 100)
+            scrollView.addSubview(imageView)
+        }
+        ADView.addSubview(scrollView)
+        ADView.addSubview(pageControl)
+        
+        constrain(ADView,pageControl){
+            
+            ADView,pageControl in
+            pageControl.left == ADView.left + 20
+            pageControl.bottom == ADView.bottom - 10
+            
+        }
+        headerView.addSubview(ADView)
+        let titleView = UIView()
+        let new = UIImageView(image: UIImage(named: "new"))
+        let label = UILabel()
+        titleView.addSubview(new)
+        titleView.addSubview(label)
+        
+        label.font = UIFont.systemFontOfSize(16)
+        label.text = "最新活动"
+        constrain(titleView,new,label) { titleView,new,label in
+            titleView.height == 40
+            titleView.width == screen.bounds.width
+            new.left == titleView.left + 20
+            new.centerY  == titleView.centerY
+            label.centerY == titleView.centerY
+            label.left == new.right + 20
+            
+        }
+        headerView.addSubview(titleView)
+        constrain(ADView,titleView) {
+        
+        ADView,titleView in
+            titleView.top  == ADView.bottom
+        
+        }
+        tableView.tableHeaderView = headerView
+        
+        
+    }
+
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
