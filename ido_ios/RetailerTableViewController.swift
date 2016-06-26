246945c05d88ff1cc69c46b7b88afce7520fc087
qimuyunduan <84607842@qq.com>
2016-06-26 11:12:11 +0800

    add retailerCollecetionView

--- ido_ios/RetailerTableViewController.swift
+++ ido_ios/RetailerTableViewController.swift
@@ -7,10 +7,36 @@
 //
 
 import UIKit
-
+import MJRefresh
+import Cartography
 
 class RetailerTableViewController: UITableViewController {
+    
+    
+    let screenSize = UIScreen.mainScreen().bounds
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setHeader(self.tableView)
+    }
+    
+    func setHeader(tableView:UITableView) -> Void {
+        let headerView = UIView(frame: CGRectMake(0,0,screenSize.width,280))
+        let collectionView = initCollectionView()
+        let gapView = UIView(frame: CGRectMake(0,0,screenSize.width,20))
+        let titleView = UIView(frame: CGRectMake(0,0,screenSize.width,40))
+        headerView.addSubview(headerView)
+        headerView.addSubview(gapView)
+        headerView.addSubview(collectionView)
+        constrain(clear: <#T##ConstraintGroup#>)
+        gapView.backgroundColor = UIColor.lightGrayColor()
+        
+        
+        
+        
+    }
+    func initCollectionView() ->UICollectionView {
+        let collectionView = UICollectionView()
+        return collectionView
+        
     }
 }
