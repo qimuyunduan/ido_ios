e4f05e6fe64b7fd324fe1fdaa4d2d241754b9d07
qimuyunduan <84607842@qq.com>
2016-06-28 19:45:06 +0800

    add constants

---
+++ ido_ios/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ido_ios
+//
+//  Created by qimuyunduan on 16/6/28.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+import Foundation
+
+let HOST = "http://localhost"
